4298f09585ec4d9086835d2ac3bdc077cc81e085
Chris <cjaneczek@student.tgm.ac.at>
2014-11-14 08:36:55 +0100

    sql files adjusted, version int

--- doc/synch-db_ER.sql
+++ doc/synch-db_ER.sql
@@ -7,6 +7,7 @@ USE synch1;
 CREATE TABLE Abteilung (
  id INT NOT NULL AUTO_INCREMENT,
  name VARCHAR(50),
+ version INT NOT NULL,
  primary key (id)
 )engine = innodb;
 
@@ -17,28 +18,29 @@ CREATE TABLE Angestellter (
  anzjahre INT,
  wohnort VARCHAR(100),
  abteilung INT,
+ version INT NOT NULL,
  primary key (id),
  FOREIGN KEY (abteilung) REFERENCES Abteilung(id)
 )engine = innodb;
 
-INSERT INTO Abteilung VALUES(1,'IT');
-INSERT INTO Abteilung VALUES(2,'MI');
-INSERT INTO Abteilung VALUES(3,'BioMed');
-INSERT INTO Abteilung VALUES(4,'CH');
-INSERT INTO Abteilung VALUES(5,'PH');
-INSERT INTO Abteilung VALUES(6,'ET');
-INSERT INTO Abteilung VALUES(7,'Mecha');
-INSERT INTO Abteilung VALUES(8,'Ind');
-INSERT INTO Abteilung VALUES(9,'WI');
-INSERT INTO Abteilung VALUES(10,'HR');
+INSERT INTO Abteilung VALUES(1,'IT', 1);
+INSERT INTO Abteilung VALUES(2,'MI', 1);
+INSERT INTO Abteilung VALUES(3,'BioMed', 1);
+INSERT INTO Abteilung VALUES(4,'CH', 1);
+INSERT INTO Abteilung VALUES(5,'PH', 1);
+INSERT INTO Abteilung VALUES(6,'ET', 1);
+INSERT INTO Abteilung VALUES(7,'Mecha', 1);
+INSERT INTO Abteilung VALUES(8,'Ind', 1);
+INSERT INTO Abteilung VALUES(9,'WI', 1);
+INSERT INTO Abteilung VALUES(10,'HR', 1);
 
-INSERT INTO Angestellter VALUES(1,'Wolfgang Mair',1000,2,'Pumperlgasse 18',1);
-INSERT INTO Angestellter VALUES(2,'Christian Janeczek',2000,1,'Burggasse 11',2);
-INSERT INTO Angestellter VALUES(3,'Stefan Stokic',4000,4,'Staugasse 28',4);
-INSERT INTO Angestellter VALUES(4,'Dominik Kodras',2200,5,'Dunkelgasse 47',3);
-INSERT INTO Angestellter VALUES(5,'Daniel Langer',1200,3,'Johannesbeergasse 4',5);
-INSERT INTO Angestellter VALUES(6,'Sebastian Mair',3100,3,'Pumperlgasse 18',7);
-INSERT INTO Angestellter VALUES(7,'Helmuth Brunner',4100,3,'Ringgasse 6',6);
-INSERT INTO Angestellter VALUES(8,'Christopher Sturz',900,1,'Springasse 16',8);
-INSERT INTO Angestellter VALUES(9,'Will Smith',1400,7,'Kalachgasse 18',9);
-INSERT INTO Angestellter VALUES(10,'Adrian Bergler',700,12,'Steigungsgasse 22',10);
+INSERT INTO Angestellter VALUES(1,'Wolfgang Mair',1000,2,'Pumperlgasse 18',1, 1);
+INSERT INTO Angestellter VALUES(2,'Christian Janeczek',2000,1,'Burggasse 11',2, 1);
+INSERT INTO Angestellter VALUES(3,'Stefan Stokic',4000,4,'Staugasse 28',4, 1);
+INSERT INTO Angestellter VALUES(4,'Dominik Kodras',2200,5,'Dunkelgasse 47',3, 1);
+INSERT INTO Angestellter VALUES(5,'Daniel Langer',1200,3,'Johannesbeergasse 4',5, 1);
+INSERT INTO Angestellter VALUES(6,'Sebastian Mair',3100,3,'Pumperlgasse 18',7, 1);
+INSERT INTO Angestellter VALUES(7,'Helmuth Brunner',4100,3,'Ringgasse 6',6, 1);
+INSERT INTO Angestellter VALUES(8,'Christopher Sturz',900,1,'Springasse 16',8, 1);
+INSERT INTO Angestellter VALUES(9,'Will Smith',1400,7,'Kalachgasse 18',9, 1);
+INSERT INTO Angestellter VALUES(10,'Adrian Bergler',700,12,'Steigungsgasse 22',10, 1);
--- doc/synch-db_ER_psql.sql
+++ doc/synch-db_ER_psql.sql
@@ -9,6 +9,7 @@ CREATE TABLE Person (
  vName VARCHAR(100) NOT NULL,
  nName VARCHAR(100) NOT NULL,
  wohnort VARCHAR(100),
+ version INT NOT NULL,
  primary key (id)
 );
 
@@ -16,28 +17,29 @@ CREATE TABLE Mitarbeiter (
  id SERIAL,
  mongehalt FLOAT(10),
  idPerson INT,
+ version INT NOT NULL,
  primary key (id),
  FOREIGN KEY (idPerson) REFERENCES Person(id)
 );
 
-INSERT INTO Person VALUES(1,'Wolfgang','Mair','Pumperlgasse 18');
-INSERT INTO Person VALUES(2,'Christian','Janeczek','Burggasse 11');
-INSERT INTO Person VALUES(3,'Stefan','Stokic','Staugasse 28');
-INSERT INTO Person VALUES(4,'Dominik','Kodras','Dunkelgasse 47');
-INSERT INTO Person VALUES(5,'Daniel','Langer','Johannesbeergasse 4');
-INSERT INTO Person VALUES(6,'Sebastian','Mair','Pumperlgasse 18');
-INSERT INTO Person VALUES(7,'Helmuth','Brunner','Ringgasse 6');
-INSERT INTO Person VALUES(8,'Christopher','Sturz','Springasse 16');
-INSERT INTO Person VALUES(9,'Will','Smith','Kalachgasse 18');
-INSERT INTO Person VALUES(10,'Adrian','Bergler','Steigungsgasse 22');
+INSERT INTO Person VALUES(1,'Wolfgang','Mair','Pumperlgasse 18', 1);
+INSERT INTO Person VALUES(2,'Christian','Janeczek','Burggasse 11', 1);
+INSERT INTO Person VALUES(3,'Stefan','Stokic','Staugasse 28', 1);
+INSERT INTO Person VALUES(4,'Dominik','Kodras','Dunkelgasse 47', 1);
+INSERT INTO Person VALUES(5,'Daniel','Langer','Johannesbeergasse 4', 1);
+INSERT INTO Person VALUES(6,'Sebastian','Mair','Pumperlgasse 18', 1);
+INSERT INTO Person VALUES(7,'Helmuth','Brunner','Ringgasse 6', 1);
+INSERT INTO Person VALUES(8,'Christopher','Sturz','Springasse 16', 1);
+INSERT INTO Person VALUES(9,'Will','Smith','Kalachgasse 18', 1);
+INSERT INTO Person VALUES(10,'Adrian','Bergler','Steigungsgasse 22', 1);
 
-INSERT INTO Mitarbeiter VALUES(1,1000,1);
-INSERT INTO Mitarbeiter VALUES(2,2000,2);
-INSERT INTO Mitarbeiter VALUES(3,4000,3);
-INSERT INTO Mitarbeiter VALUES(4,2200,4);
-INSERT INTO Mitarbeiter VALUES(5,1200,5);
-INSERT INTO Mitarbeiter VALUES(6,3100,6);
-INSERT INTO Mitarbeiter VALUES(7,4100,7);
-INSERT INTO Mitarbeiter VALUES(8,900,8);
-INSERT INTO Mitarbeiter VALUES(9,1400,9);
-INSERT INTO Mitarbeiter VALUES(10,700,10);
+INSERT INTO Mitarbeiter VALUES(1,1000,1,1);
+INSERT INTO Mitarbeiter VALUES(2,2000,2,1);
+INSERT INTO Mitarbeiter VALUES(3,4000,3,1);
+INSERT INTO Mitarbeiter VALUES(4,2200,4,1);
+INSERT INTO Mitarbeiter VALUES(5,1200,5,1);
+INSERT INTO Mitarbeiter VALUES(6,3100,6,1);
+INSERT INTO Mitarbeiter VALUES(7,4100,7,1);
+INSERT INTO Mitarbeiter VALUES(8,900,8,1);
+INSERT INTO Mitarbeiter VALUES(9,1400,9,1);
+INSERT INTO Mitarbeiter VALUES(10,700,10,1);
